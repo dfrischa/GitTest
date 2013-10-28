4d3b1b85764952f9ca9bd9ecab6c848e9da0ded3
Dirk Frischalowski <d.frischalowski@itsonix.com>
2013-10-28 16:42:47 +0100

    Initial Commit

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<DoSomething> liste = new List<DoSomething>();
+			liste.Add(new DoSomething(134));
+			liste.Add(new DoSomething(579, () => { return "dos werd isse: 579"; }));
+
+			foreach(DoSomething dos in liste)
+			{
+				dos.ReturnValue();
+			}
+		}
+	}
+
+	public class DoSomething
+	{
+		private int _value;
+
+		private Func<string> _returnValue;
+
+		public DoSomething(int value, Func<string> newFunc = null)
+		{
+			_value = value;
+
+			if (newFunc == null)
+				_returnValue = () => { return "The value is: " + _value; };
+			else
+				_returnValue = newFunc;
+		}
+
+		public string ReturnValue()
+		{
+			return _returnValue();
+		}
+	}
+}
